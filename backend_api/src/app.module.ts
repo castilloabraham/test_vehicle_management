bb31b24e7731d968aa19406ee26796c88ded5069
castilloabraham <adcdabrahamcastillo@gmail.com>
2025-02-14 03:19:01 -0400

    chore: configuracion de modulos y conexion a la db

--- backend_api/src/app.module.ts
+++ backend_api/src/app.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { VehiclesModule } from './vehicles/vehicles.module';
 
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [VehiclesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    VehiclesModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT ?? '5432'),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- backend_api/src/vehicles/vehicles.module.ts
+++ backend_api/src/vehicles/vehicles.module.ts
@@ -2,7 +2,12 @@ import { Module } from '@nestjs/common';
 import { VehiclesService } from './vehicles.service';
 import { VehiclesController } from './vehicles.controller';
 
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Vehicle } from './entities/vehicle.entity';
+
 @Module({
+  imports: [TypeOrmModule.forFeature([Vehicle])],
   controllers: [VehiclesController],
   providers: [VehiclesService],
 })
